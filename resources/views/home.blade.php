b3accd5da6a9deddd4f0e745ffe187e07bc1cdd9
Yismen Jorge <yismen.jorge@gmail.com>
2020-06-03 06:15:29 -0400

    ..

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <timy-user-dashbord></timy-user-dashbord>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate praesentium consequuntur, possimus atque cum dicta, sint exercitationem quo nihil illum minus asperiores sit temporibus vero qui laborum reiciendis quis itaque?
         </div>
     </div>
 </div>
